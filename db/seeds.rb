b7eed4107a0f210d8da0b22a6343117b2e668b0d
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-13 14:16:04 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "123", store: "walmart")
+Coupon.create(coupon_code: "234", store: "target")
+Coupon.create(coupon_code: "567", store: "gamestop")
