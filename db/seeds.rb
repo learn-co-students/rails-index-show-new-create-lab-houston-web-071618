1e85072fbb9d22a6d3257a1f86471ac5647a34bb
pabloargueta <pabloargueta27@gmail.com>
2018-08-14 00:26:59 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "12345", store: "five guys")
+Coupon.create(coupon_code: "13423", store: "pizza hut")
+Coupon.create(coupon_code: "213412", store: "mcdonalds")
+Coupon.create(coupon_code: "123412", store: "burger king")
+Coupon.create(coupon_code: "577856", store: "jack")
+Coupon.create(coupon_code: "908798", store: "express")
+Coupon.create(coupon_code: "5676734", store: "walmart")
+Coupon.create(coupon_code: "2346787", store: "ross")
