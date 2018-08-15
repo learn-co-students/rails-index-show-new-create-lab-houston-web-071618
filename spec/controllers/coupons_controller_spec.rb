16ca26885f208995d8f522a280bd37a3dcb3daa6
kevinptx <kevin.peery@gmail.com>
2018-08-15 08:58:07 -0500

    Done.

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -1,17 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe CouponsController, type: :controller do
-
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
   end
@@ -22,5 +14,4 @@ RSpec.describe CouponsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
